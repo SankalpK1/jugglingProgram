ColorCopy is correct file, created Freeze, Ranges, Averages of HSVs, identified ellipses and found their centers.

colorCopy2.py
@@ -4,31 +4,38 @@ import numpy as np
 
 cam = cv2.VideoCapture(0)
 
-
-
 global img
+
+numClicked = 0
 
 numBalls = 0
 
 hsv_values = []
 
+hsv_values2 = []
+
+hsv_ranges = []
+
 def onmouse(event, x, y, flags, param):
     global numBalls
-
+    global numClicked
     if event == cv2.EVENT_LBUTTONDOWN:
         img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         (h,s,v) = cv2.split(img2)
         print(h[y][x])
         # print(countSelected)
-        hsv_values.append((h[y][x],s[y][x],v[y][x]))
-        print (hsv_values)
-        numBalls+=1
+        hsv_values.append([h[y][x],s[y][x],v[y][x]])
+        numClicked+=1
+        if numClicked == 3:
+            numBalls+=1
+            numClicked = 0
 
 
 while True:
-    __,img = cam.read()
-    img = cv2.resize(img,(1000, 563))
-    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    if numClicked < 1:
+        __,img = cam.read()
+        img = cv2.resize(img, (1000, 563))
+        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
     cv2.imshow('juggling', img)
     cv2.setMouseCallback('juggling', onmouse)
     cv2.waitKey(10)
@@ -40,7 +47,7 @@ while True:
 
 
 def hueUpper (hue):
-    if hue<164:
+    if hue<179-15:
         return int (hue)+15
     else:
         return 179
@@ -50,13 +57,13 @@ def hueLower (hue):
     else:
         return 0
 def satValUpper (satVal):
-    if satVal<225:
-        return int (satVal)+30
+    if satVal<255-20:
+        return int (satVal)+20
     else:
         return 255
 def satValLower (satVal):
-    if satVal>30:
-        return int (satVal)-30
+    if satVal>20:
+        return int (satVal)-20
     else:
         return 0
 
@@ -70,8 +77,11 @@ while True:
     mask=np.zeros((img.shape[0],img.shape[1],1), np.uint8)
 
     for values in hsv_values:
-        print(values[0])
-        threshold = cv2.inRange(hsv, (hueLower(values[0]), (satValLower(values[1])), 0), (hueUpper(values[0]),(satValUpper(values[1])), 255))
+        kernel = np.ones((7, 7), np.uint8)
+        hsv_filtered = cv2.morphologyEx(hsv, cv2.MORPH_OPEN, kernel)
+        hsv_filtered2 = cv2.blur(hsv_filtered, (15,15))
+        threshold = cv2.inRange(hsv, (hueLower(values[0]), (satValLower(values[1])), 0),
+                                (hueUpper(values[0]), (satValUpper(values[1])), 255))
 
 
         mask=cv2.bitwise_or(mask,threshold)