b9aaca44ada3c7654f72055e3f0414c225d5a21f
goldhaberz <zevgoldhabergordon@gmail.com>
2019-07-23 15:26:17 -0500

    Color assignment fix

--- colorCopy.py
+++ colorCopy.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import math
 import colorsys
-from window_info import *
+
 
 cam = cv2.VideoCapture(0)
 
@@ -23,6 +23,16 @@ hsv_ranges = []
 positions = []
 
 existsLine = 0
+
+xVelocity = []
+
+yVelocity = []
+
+xVelocityPrev = []
+
+yVelocityPrev = []
+
+numThrown = 0
 
 def onmouse(event, x, y, flags, param):
     global numBalls
@@ -49,8 +59,25 @@ def onmouse(event, x, y, flags, param):
             numClicked = 0
             positions.append([[0, 0]])
             frameNum.append(0)
+            xVelocity.append(0)
+            yVelocity.append(0)
+            xVelocityPrev.append(0)
+            yVelocityPrev.append(0)
 
 
+while True:
+    if numClicked < 1:
+        __,img = cam.read()
+        img = cv2.resize(img, (1000, 563))
+        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imshow('juggling', img)
+    cv2.setMouseCallback('juggling', onmouse)
+    cv2.waitKey(10)
+    ch = chr(0xFF & cv2.waitKey(5))
+    if ch == 'q':
+        exit(0)
+    elif ch ==' ':
+        break
 
 
 def hueUpper (hue, range):
@@ -74,126 +101,94 @@ def satValLower (satVal, range):
     else:
         return 0
 
-def main():
-    global cam
+while True:
+    _,img=cam.read()
+    img=cv2.resize(img,(1000,563))
+    img=cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    (height,width,depth) = img.shape
+    fakeImage = np.zeros((height, width, depth), np.uint8)
+    # threshold = cv2.inRange(hsv, (101, 129, 22), (115, 255, 255))
+    mask=np.zeros((img.shape[0],img.shape[1],1), np.uint8)
 
-    global img
+    for values in range(numBalls):
+        kernel = np.ones((11, 11), np.uint8)
+        # hsv_filtered = cv2.morphologyEx(hsv, cv2.MORPH_OPEN, kernel)
+        # hsv_filtered2 = cv2.GaussianBlur(hsv_filtered, (17,17), 0)
+        threshold = cv2.inRange(hsv, (hueLower(hsv_values2[values][0], hsv_ranges[3*values]),
+                                                (satValLower(hsv_values2[values][1], hsv_ranges[3*values+1])), 0),
+                                                (hueUpper(hsv_values2[values][0], hsv_ranges[3*values]),(satValUpper(hsv_values2[values][1],  hsv_ranges[3*values+1])), 255))
+        threshold_filtered = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+        existsLine = 0
+        contrs, hier = cv2.findContours(threshold_filtered, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contrs) != 0:
+            for i in range(len(contrs)):
+                if len(contrs[i]) >= 15:
+                    existsLine = 1
+                    img3 = img
+                    hsv_filtered = cv2.morphologyEx(img3, cv2.MORPH_OPEN, kernel)
+                    cv2.drawContours(hsv_filtered, contrs, -1, (150, 10, 255), 3)
+                    ellipse = cv2.fitEllipse(contrs[i])
+                    # cv2.ellipse(img, ellipse, (0, 255, 0), 2)
+                    # cv2.circle(img, (int((int(ellipse[0][1])+int(ellipse[1][1]))/2), int((int(ellipse[0][0])+int(ellipse[1][0]))/2)), 20, (255, 255, 255))
+                    # cv2.circle(img, (int(ellipse[0][0]), int(ellipse[0][1])), 20, (255, 255, 255))
+                    positions[values].append([int(ellipse[0][0]), int(ellipse[0][1])])
+                    frameNum[values] += 1
+                    if frameNum[values] >= 2:
+                        xVelocityPrev[values] = positions[values][frameNum[values]-1][0] - positions[values][frameNum[values]-2][0]
+                        xVelocity[values] = positions[values][frameNum[values]][0] - positions[values][frameNum[values] - 1][0]
+                        yVelocityPrev[values] = positions[values][frameNum[values] - 1][1] - positions[values][frameNum[values] - 2][1]
+                        yVelocity[values] = positions[values][frameNum[values]][1] - positions[values][frameNum[values] - 1][1]
+                        totalVelocityPrev = math.sqrt(xVelocityPrev[values]*xVelocityPrev[values] + yVelocityPrev[values]*yVelocityPrev[values])
+                        totalVelocity = math.sqrt(xVelocity[values] * xVelocity[values] + yVelocity[values] * yVelocity[values])
+                        (height, width, depth) = img.shape
+                        nonImage = np.zeros((height, width, depth), np.uint8)
+                        # print (xVelocity[values])
+                        if yVelocity[values] < 0 and yVelocityPrev[values] > 0:
+                            numThrown+=1
+                            print (numThrown)
+                        for j in range(2, frameNum[values]):
+                            lineColor = np.uint8(
+                                [[[hsv_values2[values][0], hsv_values2[values][1], hsv_values2[values][2]]]])
+                            #
+                            # print(cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)[0][0][0])
+                            bgrColor = cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)
+                            actualbgr = bgrColor[0][0]
+                            aaa = tuple([int(x) for x in actualbgr])
+                            cv2.line(nonImage, (positions[values][j - 1][0], positions[values][j - 1][1]),
+                                     (positions[values][j][0], positions[values][j][1]),
+                                    aaa , thickness=5)
+                        fakeImage = cv2.addWeighted(fakeImage, 1, nonImage, 1/3, 0)
+                    break
+        if existsLine == 0:
+            if frameNum[values] >= 2:
+                (height, width, depth) = img.shape
+                nonImage = np.zeros((height, width, depth), np.uint8)
+                for j in range(2, frameNum[values]):
+                    lineColor = np.uint8(
+                        [[[hsv_values2[values][0], hsv_values2[values][1], hsv_values2[values][2]]]])
+                    #
+                    # print(cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)[0][0][0])
+                    bgrColor = cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)
+                    actualbgr = bgrColor[0][0]
+                    aaa = tuple([int(x) for x in actualbgr])
+                    cv2.line(nonImage, (positions[values][j - 1][0], positions[values][j - 1][1]),
+                             (positions[values][j][0], positions[values][j][1]),
+                             aaa, thickness=5)
+                fakeImage = cv2.addWeighted(fakeImage, 1, nonImage, 1 / 3, 0)
+    img = cv2.addWeighted(fakeImage, 0.9, img, 0.1, 100)
+                # else:
+                #     # optional to "delete" the small contours
+                #     cv2.drawContours(img, contrs, -1,  (0, 0, 0), -1)1
+    #     cv2.drawContours(img, contrs, -1, (0, 255, 0), 3)
+    #     mask=cv2.bitwise_or(mask,threshold)
+    #
+    # maskedimg=cv2.bitwise_and(img,img,mask=mask)
 
-    global frameNum
-
-    global numClicked
-
-    global numBalls
-
-    global hsv_values
-
-    global hsv_values2
-
-    global hsv_ranges
-
-    global positions
-
-    global existsLine
-    cam = cv2.VideoCapture(0)
-
-    frameNum = []
-
-    numClicked = 0
-
-    numBalls = 0
-
-    hsv_values = []
-
-    hsv_values2 = []
-
-    hsv_ranges = []
-
-    positions = []
-
-    existsLine = 0
-    while True:
-        if numClicked < 1:
-            __,img = cam.read()
-            img = cv2.resize(img, hgtwid())
-            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-        cv2.imshow(prgmName(), img)
-        cv2.setMouseCallback(prgmName(), onmouse)
-        cv2.waitKey(1)
-        ch = chr(0xFF & cv2.waitKey(5))
-        if ch == 'q':
-            return
-        elif ch ==' ':
-            break
-
-    while True:
-        _,img=cam.read()
-        img=cv2.resize(img,(hgtwid()))
-        img=cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-        hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
-        # threshold = cv2.inRange(hsv, (101, 129, 22), (115, 255, 255))
-        mask=np.zeros((img.shape[0],img.shape[1],1), np.uint8)
-
-        for values in range(numBalls):
-            kernel = np.ones((3, 3), np.uint8)
-            # hsv_filtered = cv2.morphologyEx(hsv, cv2.MORPH_OPEN, kernel)
-            # hsv_filtered2 = cv2.GaussianBlur(hsv_filtered, (17,17), 0)
-            threshold = cv2.inRange(hsv, (hueLower(hsv_values2[values][0], hsv_ranges[3*values]),
-                                                    (satValLower(hsv_values2[values][1], hsv_ranges[3*values+1])), 0),
-                                                    (hueUpper(hsv_values2[values][0], hsv_ranges[3*values]),(satValUpper(hsv_values2[values][1],  hsv_ranges[3*values+1])), 255))
-            threshold_filtered = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
-            existsLine = 0
-            contrs, hier = cv2.findContours(threshold_filtered, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if len(contrs) != 0:
-                for i in range(len(contrs)):
-                    if len(contrs[i]) >= 15:
-                        existsLine = 1
-                        cv2.drawContours(img, contrs, -1, (150, 10, 255), 3)
-                        ellipse = cv2.fitEllipse(contrs[i])
-                        cv2.ellipse(img, ellipse, (0, 255, 0), 2)
-                        # cv2.circle(img, (int((int(ellipse[0][1])+int(ellipse[1][1]))/2), int((int(ellipse[0][0])+int(ellipse[1][0]))/2)), 20, (255, 255, 255))
-                        # cv2.circle(img, (int(ellipse[0][0]), int(ellipse[0][1])), 20, (255, 255, 255))
-                        positions[values].append([int(ellipse[0][0]), int(ellipse[0][1])])
-                        frameNum[values] += 1
-                        if frameNum[values] >= 2:
-                            for j in range(2, frameNum[values]):
-                                lineColor = np.uint8(
-                                    [[[hsv_values2[values][0], hsv_values2[values][1], hsv_values2[values][2]]]])
-                                #
-                                # print(cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)[0][0][0])
-                                bgrColor = cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)
-                                actualbgr = bgrColor[0][0]
-                                aaa = tuple([int(x) for x in actualbgr])
-                                cv2.line(img, (positions[values][j - 1][0], positions[values][j - 1][1]),
-                                         (positions[values][j][0], positions[values][j][1]),
-                                        aaa , thickness=5)
-                        break
-            if existsLine == 0:
-                if frameNum[values] >= 2:
-                    for j in range(2, frameNum[values]):
-                        lineColor = np.uint8(
-                            [[[hsv_values2[values][0], hsv_values2[values][1], hsv_values2[values][2]]]])
-                        #
-                        # print(cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)[0][0][0])
-                        bgrColor = cv2.cvtColor(lineColor, cv2.COLOR_HSV2BGR)
-                        actualbgr = bgrColor[0][0]
-                        aaa = tuple([int(x) for x in actualbgr])
-                        cv2.line(img, (positions[values][j - 1][0], positions[values][j - 1][1]),
-                                 (positions[values][j][0], positions[values][j][1]),
-                                 aaa, thickness=5)
-
-                    # else:
-                    #     # optional to "delete" the small contours
-                    #     cv2.drawContours(img, contrs, -1,  (0, 0, 0), -1)1
-            # cv2.drawContours(img, contrs, -1, (0, 255, 0), 3)
-            # mask=cv2.bitwise_or(mask,threshold)
-
-        maskedimg=cv2.bitwise_and(img,img,mask=mask)
-
-        cv2.imshow(prgmName(), img)
-        ch = chr(0xFF & cv2.waitKey(1))
-        if ch == 'q':
-            break
+    cv2.imshow("wow", img)
+    ch = chr(0xFF & cv2.waitKey(1))
+    if ch == 'q':
+        break
 
 
 
